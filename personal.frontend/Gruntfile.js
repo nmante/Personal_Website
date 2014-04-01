d5d286e18678fb2c6833f731944d6cf3a5b800c9
nmante <nmante88@gmail.com>
2014-04-01 01:05:30 -0700

    Ostrich Sans working, need to add ostrich-sans dashed

--- personal.frontend/Gruntfile.js
+++ personal.frontend/Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function ( grunt ) {
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-ngmin');
   grunt.loadNpmTasks('grunt-html2js');
-  //grunt.loadTasks('grunt-typopro.js');
+  grunt.loadTasks('tasks');
   /**
    * Load in our build configuration file.
    */
