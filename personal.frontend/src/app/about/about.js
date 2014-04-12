c070d93bcf04ff88f52ef60826bc93d14acc3ed4
nmante <nmante88@gmail.com>
2014-04-11 23:49:49 -0700

    About.js, rearranging code section to column layout

--- personal.frontend/src/app/about/about.js
+++ personal.frontend/src/app/about/about.js
@@ -22,7 +22,7 @@ angular.module('personalWebApp.about', [
 .controller('AboutCtrl', ['$scope', function($scope){
 	$scope.bootstrapColumns = 12;
 	$scope.aboutSection = {
-		"title" : "Student",
+		"title" : "I <i>learn</i> things...",
 		"rows" : [
 			{
 				"cols" : [
@@ -30,7 +30,7 @@ angular.module('personalWebApp.about', [
 						"header" : "The <b><i>Present</i></b>",
 						"paragraphs" : [
 							"I'm currently a <b>Biomedical Engineering</b> <strong>PhD</strong> student at the <a target=\"_blank\" href=\"http://www.usc.edu\">University of Southern California</a> working in the same lab that's making <a target=\"_blank\" href=\"http://cbsn.ws/1hyv5zH\">Retinal Prosthesis</a> possible. I'm also pursuing a <strong>Master's</strong> degree in <b>Computer Science</b>.",
-							"My work is centered around making blind people's lives easier. The main goal of my project is to allow blind people to find things that are important to them. For the tech-savvy, this includes <strong>Computer Vision</strong> and <strong>Speech Synthesis</strong>. Check it out <a href=\"#/projects\">here</a>."
+							"My work is centered around making blind people's lives easier. The main goal of my project is to build a system that allows blind people to find things that are important to them. For the tech-savvy, this includes <strong>Computer Vision</strong> and <strong>Speech Synthesis</strong>. Check it out <a href=\"#/projects\">here</a>."
 						]
 					},
 					{
@@ -48,7 +48,7 @@ angular.module('personalWebApp.about', [
 	};
 
 	$scope.coderSection = {
-		"title" : "Programmer", 
+		"title" : "I <i>code</i> as well...", 
 		"rows" : [
 			{
 				"cols" : [
@@ -57,17 +57,18 @@ angular.module('personalWebApp.about', [
 						"paragraphs" : [
 							"My journey as a developer started in 2010 with <b>C++</b>. After a year long hiatus from coding, I picked it up again in 2011 and haven't looked back. I've worked in a variety of languages ranging from frontend to backend technologies."
 						]
-					}
-				]
-			},
-			{
-				"cols" : [
+					},
 					{
 						"header" : "Mobile - Web - Backend",
 						"paragraphs" : [
 							"I've done iOS" 
 						]
 					}
+				]
+			},
+			{
+				"cols" : [
+					
 				]
 			}
 		]
@@ -119,60 +120,62 @@ angular.module('personalWebApp.about', [
 
 	];
 
-	var canvas=document.getElementById("canvas");
+	function drawEntireHexDiv(){
+		var canvas=document.getElementById("canvas");
 
-	var cxt = canvas.getContext('2d');
-	var hexRadius = 40;
-	var hexDiameter = 2*hexRadius;
-	var gap = 3;
-	var numberOfColumns = 5;
+		var cxt = canvas.getContext('2d');
+		var hexRadius = 40;
+		var hexDiameter = 2*hexRadius;
+		var gap = 3;
+		var numberOfColumns = 5;
 
-	canvas.width = numberOfColumns*(3/4*hexDiameter + gap) + hexRadius;
-	canvas.height = (hexRadius)*(numberOfColumns + 1);
-	// hexagon
-	function drawHex(xCenter, yCenter, mSize, mColor){
-		var numberOfSides = 6,
-		size = mSize,
-		Xcenter = xCenter,
-		Ycenter = yCenter;
+		canvas.width = numberOfColumns*(3/4*hexDiameter + gap) + hexRadius;
+		canvas.height = (hexRadius)*(numberOfColumns + 1);
+		// hexagon
+		function drawHex(xCenter, yCenter, mSize, mColor){
+			var numberOfSides = 6,
+			size = mSize,
+			Xcenter = xCenter,
+			Ycenter = yCenter;
 
-		cxt.beginPath();
-		cxt.moveTo (Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0));          
+			cxt.beginPath();
+			cxt.moveTo (Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0));          
 
-		for (var i = 1; i <= numberOfSides;i += 1) {
-		cxt.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+			for (var i = 1; i <= numberOfSides;i += 1) {
+			cxt.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+			}
+
+			cxt.strokeStyle = "ccc";
+			cxt.lineWidth = 1;
+			cxt.fillStyle = mColor;
+			cxt.fill();
+			cxt.stroke();
 		}
 
-		cxt.strokeStyle = "ccc";
-		cxt.lineWidth = 1;
-		cxt.fillStyle = mColor;
-		cxt.fill();
-		cxt.stroke();
-	}
+		var polNumber = 0;
+		for (var row = 0; row < numberOfColumns; row++) {
+			for (var col = 0; col < numberOfColumns; col++) {
+				if ((row+col)%2 === 0 && (row+col) !== 0 && (row+col) !== 2*(numberOfColumns - 1)
+					) {
 
-	var polNumber = 0;
-	for (var row = 0; row < numberOfColumns; row++) {
-		for (var col = 0; col < numberOfColumns; col++) {
-			if ((row+col)%2 === 0 && (row+col) !== 0 && (row+col) !== 2*(numberOfColumns - 1)
-				) {
+					console.log("row: " + row + "col: " + col);
 
-				console.log("row: " + row + "col: " + col);
-
-				if ((row === numberOfColumns-1 && col === 0) || (col === numberOfColumns - 1 && row === 0)) {
-					break;
+					if ((row === numberOfColumns-1 && col === 0) || (col === numberOfColumns - 1 && row === 0)) {
+						break;
+					}
+					//We can draw now
+					
+					var xCenter = (hexRadius) + col*(3/4*(hexDiameter)+ gap);
+					var yCenter = (hexRadius)*(row + 1);
+					drawHex(xCenter, yCenter, hexRadius, $scope.languages[polNumber].color);
+					polNumber++;
 				}
-				//We can draw now
-				
-				var xCenter = (hexRadius) + col*(3/4*(hexDiameter)+ gap);
-				var yCenter = (hexRadius)*(row + 1);
-				drawHex(xCenter, yCenter, hexRadius, $scope.languages[polNumber].color);
-				polNumber++;
 			}
 		}
+		var lastXCenter = (hexRadius) + 1/2*(numberOfColumns-1)*(3/4*(hexDiameter)+ gap);
+		var lastYCenter = (hexRadius)*(numberOfColumns);
+		var len = $scope.languages.length;
+		drawHex(lastXCenter, lastYCenter, hexRadius, $scope.languages[len - 1].color);
 	}
-	var lastXCenter = (hexRadius) + 1/2*(numberOfColumns-1)*(3/4*(hexDiameter)+ gap);
-	var lastYCenter = (hexRadius)*(numberOfColumns);
-	var len = $scope.languages.length;
-	drawHex(lastXCenter, lastYCenter, hexRadius, $scope.languages[len - 1].color);
 
 }]);
